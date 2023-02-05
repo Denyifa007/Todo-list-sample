c24adc12f355148a313855c63211f92ee8fe0071
denyifa007 <denyifasamuel@hotmail.com>
2023-02-05 21:18:32 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,72 @@
+'use strict'
+window.addEventListener('load', () => {
+    const form = document.querySelector('#taskListForm');
+    const input = document.querySelector('#newTaskInput');
+    const listElement = document.querySelector('#tasks');
+    form.addEventListener('submit', (e) =>{
+        e.preventDefault();
+
+            const task = input.value;    
+
+            if (!task) {
+                alert("fill out the task");
+                return;
+                
+            }
+            const taskElement = document.createElement('div')
+            taskElement.classList.add('task');
+
+            const taskContentElemnet = document.createElement('div');
+            taskContentElemnet.classList.add('content');
+            
+
+            taskElement.appendChild(taskContentElemnet);
+
+            const taskInputElement = document.createElement('input');
+            taskInputElement.classList.add('text');
+            taskInputElement.type ='text';
+            taskInputElement.value = task;
+            taskInputElement.setAttribute('readonly', 'readonly');
+
+            taskContentElemnet.appendChild(taskInputElement);
+
+            const taskActionsElement = document.createElement('div');
+            taskActionsElement.classList.add('actions');
+
+            const taskEditElement = document.createElement('button');
+            taskEditElement.classList.add('edit');
+            taskEditElement.innerHTML = 'Edit';
+
+            const taskDeleteElement = document.createElement('button');
+            taskDeleteElement.classList.add('delete')
+            taskDeleteElement.innerHTML = 'Delete';
+            
+
+            taskActionsElement.appendChild(taskEditElement);
+            taskActionsElement.appendChild(taskDeleteElement)
+
+            taskElement.appendChild(taskActionsElement)
+            listElement.appendChild(taskElement);
+            input.value = '';
+
+            taskEditElement.addEventListener('click', ()=> {
+                if (taskEditElement.innerText.toLowerCase() == 'edit') {
+                    taskInputElement.removeAttribute('readonly');
+                    taskInputElement.focus()
+                    taskEditElement.innerText = 'Save';
+
+                    
+                }else{
+                    taskInputElement.setAttribute('readonly', 'readonly');
+                    taskEditElement.innerText = 'Edit'
+                }
+            })
+            
+            taskDeleteElement.addEventListener('click', ()=> {
+                listElement.removeChild(taskElement);
+            })
+           
+    })
+    
+
+})
